92f6c2b72dbe735797cff070cd5088651a8cfb9d
austin-keith-vigo <austin.keith.vigo@gmail.com>
2021-07-28 14:26:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 
+console.log("Starting server")
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
